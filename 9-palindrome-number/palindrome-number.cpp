009e1ac0bcf050dc788f6639ac1f1825decab93c
BILAL <mdbilal6450@gmail.com>
2026-08-22 19:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)
-        return 0;
-
-        int ans=0,rem;
-        int num=x;
-
-        while(x!=0){
-            rem = x%10;
-
-            if(ans>INT_MAX/10 || ans<INT_MIN/10)
-            return 0;
-
-            ans = ans*10 + rem;
-            x=x/10;
+        if (x < 0) return false;
+        if (x >= 0 && x < 10) return true;
+        long int reversed = 0;
+        int temp = x;  
+        while (temp > 0) {
+            int digit = temp % 10;     
+            temp /= 10;                 
+            reversed = reversed * 10 + digit; 
         }
-        if(num==ans)
-        return 1;
-        else
-        return 0;
+        return x == reversed;
     }
 };
