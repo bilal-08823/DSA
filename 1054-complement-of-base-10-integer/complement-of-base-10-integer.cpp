88d2b1204709abde231f64d489334c2b550ee839
BILAL <mdbilal6450@gmail.com>
2026-08-23 09:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.66%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        int mask = 0;
+
+        while (n > mask) {
+            mask = (mask << 1) | 1;
+        }
+
+        return n ^ mask;
+    }
+};
